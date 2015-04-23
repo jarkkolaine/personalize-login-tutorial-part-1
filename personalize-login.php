fd66d563964f8cbf4d335121ffe022c53de7d708
Jarkko Laine <jarkko@jarkkolaine.com>
2015-04-23 14:13:09 +0300

    first commit

---
+++ personalize-login.php
@@ -0,0 +1,284 @@
+<?php
+/**
+ * Plugin Name:       Personalize Login
+ * Description:       A plugin that replaces the WordPress login flow with a custom page.
+ * Version:           1.0.0
+ * Author:            Jarkko Laine
+ * License:           GPL-2.0+
+ * Text Domain:       personalize-login
+ */
+
+class Personalize_Login_Plugin {
+
+	/**
+	 * Initializes the plugin.
+	 *
+	 * To keep the initialization fast, only add filter and action
+	 * hooks in the constructor.
+	 */
+	public function __construct() {
+		add_action( 'login_form_login', array( $this, 'redirect_to_custom_login' ) );
+
+		add_action( 'wp_logout', array( $this, 'redirect_after_logout' ) );
+
+		add_filter( 'authenticate', array( $this, 'maybe_redirect_at_authenticate' ), 101, 3 );
+		add_filter( 'login_redirect', array( $this, 'redirect_after_login' ), 10, 3 );
+
+		add_shortcode( 'custom-login-form', array( $this, 'render_login_form' ) );
+	}
+
+	/**
+	 * Plugin activation hook.
+	 *
+	 * Creates all WordPress pages needed by the plugin.
+	 */
+	public static function plugin_activated() {
+		// Information needed for creating the plugin's pages
+		$page_definitions = array(
+			'member-login' => array(
+				'title' => __( 'Sign In', 'personalize-login' ),
+				'content' => '[custom-login-form]'
+			),
+			'member-account' => array(
+				'title' => __( 'Your Account', 'personalize-login' ),
+				'content' => '[account-info]'
+			),
+		);
+
+		foreach ( $page_definitions as $slug => $page ) {
+			// Check that the page doesn't exist already
+			$query = new WP_Query( 'pagename=' . $slug );
+			if ( ! $query->have_posts() ) {
+				// Add the page using the data from the array above
+				wp_insert_post(
+					array(
+						'post_content'   => $page['content'],
+						'post_name'      => $slug,
+						'post_title'     => $page['title'],
+						'post_status'    => 'publish',
+						'post_type'      => 'page',
+						'ping_status'    => 'closed',
+						'comment_status' => 'closed',
+					)
+				);
+			}
+		}
+	}
+
+	//
+	// REDIRECT FUNCTIONS
+	//
+
+	/**
+	 * Redirect the user to the custom login page instead of wp-login.php.
+	 */
+	public function redirect_to_custom_login() {
+		if ( $_SERVER['REQUEST_METHOD'] == 'GET' ) {
+			if ( is_user_logged_in() ) {
+				// Already logged in users should go straight to the account page
+				wp_redirect( home_url( 'member-account' ) );
+				exit;
+			}
+
+			// The rest are redirected to the login page
+			$login_url = home_url( 'member-login' );
+			if ( ! empty( $_REQUEST['redirect_to'] ) ) {
+				$login_url = add_query_arg( 'redirect_to', $_REQUEST['redirect_to'], $login_url );
+			}
+
+			wp_redirect( $login_url );
+			exit;
+		}
+	}
+
+	/**
+	 * Returns the URL to which the user should be redirected after the (successful) login.
+	 *
+	 * @param string           $redirect_to           The redirect destination URL.
+	 * @param string           $requested_redirect_to The requested redirect destination URL passed as a parameter.
+	 * @param WP_User|WP_Error $user                  WP_User object if login was successful, WP_Error object otherwise.
+	 *
+	 * @return string Redirect URL
+	 */
+	public function redirect_after_login( $redirect_to, $requested_redirect_to, $user ) {
+		$redirect_url = home_url();
+
+		if ( ! isset( $user->ID ) ) {
+			return $redirect_url;
+		}
+
+		if ( user_can( $user, 'manage_options' ) ) {
+			// Use the redirect_to parameter if one is set, otherwise redirect to admin dashboard.
+			if ( $requested_redirect_to == '' ) {
+				$redirect_url = admin_url();
+			} else {
+				$redirect_url = $redirect_to;
+			}
+		} else {
+			// Non-admin users always go to their account page after login
+			$redirect_url = home_url( 'member-account' );
+		}
+
+		return wp_validate_redirect( $redirect_url, home_url() );
+	}
+
+	/**
+	 * Redirect to custom login page after the user has been logged out.
+	 */
+	public function redirect_after_logout() {
+		$redirect_url = home_url( 'member-login?logged_out=true' );
+		wp_safe_redirect( $redirect_url );
+		exit;
+	}
+
+	/**
+	 * Redirect the user after authentication if there were any errors.
+	 *
+	 * @param Wp_User|Wp_Error  $user       The signed in user, or the errors that have occurred during login.
+	 * @param string            $username   The user name used to log in.
+	 * @param string            $password   The password used to log in.
+	 *
+	 * @return Wp_User|Wp_Error The logged in user, or error information if there were errors.
+	 */
+	public function maybe_redirect_at_authenticate( $user, $username, $password ) {
+		// Check if the earlier authenticate filter (most likely,
+		// the default WordPress authentication) functions have found errors
+		if ( $_SERVER['REQUEST_METHOD'] === 'POST' ) {
+			if ( is_wp_error( $user ) ) {
+				$error_codes = join( ',', $user->get_error_codes() );
+
+				$login_url = home_url( 'member-login' );
+				$login_url = add_query_arg( 'login', $error_codes, $login_url );
+
+				wp_redirect( $login_url );
+				exit;
+			}
+		}
+
+		return $user;
+	}
+
+
+	//
+	// FORM RENDERING SHORTCODES
+	//
+
+	/**
+	 * A shortcode for rendering the login form.
+	 *
+	 * @param  array   $attributes  Shortcode attributes.
+	 * @param  string  $content     The text content for shortcode. Not used.
+	 *
+	 * @return string  The shortcode output
+	 */
+	public function render_login_form( $attributes, $content = null ) {
+		// Parse shortcode attributes
+		$default_attributes = array( 'show_title' => false );
+		$attributes = shortcode_atts( $default_attributes, $attributes );
+
+		if ( is_user_logged_in() ) {
+			return __( 'You are already signed in.', 'personalize-login' );
+		}
+
+		// Pass the redirect parameter to the WordPress login functionality: by default,
+		// don't specify a redirect, but if a valid redirect URL has been passed as
+		// request parameter, use it.
+		$attributes['redirect'] = '';
+		if ( isset( $_REQUEST['redirect_to'] ) ) {
+			$attributes['redirect'] = wp_validate_redirect( $_REQUEST['redirect_to'], $attributes['redirect'] );
+		}
+
+		// Error messages
+		$errors = array();
+		if ( isset( $_REQUEST['login'] ) ) {
+			$error_codes = explode( ',', $_REQUEST['login'] );
+
+			foreach ( $error_codes as $code ) {
+				$errors []= $this->get_error_message( $code );
+			}
+		}
+		$attributes['errors'] = $errors;
+
+		// Check if user just logged out
+		$attributes['logged_out'] = isset( $_REQUEST['logged_out'] ) && $_REQUEST['logged_out'] == true;
+
+		// Render the login form using an external template
+		return $this->get_template_html( 'login_form', $attributes );
+	}
+
+	/**
+	 * Renders the contents of the given template to a string and returns it.
+	 *
+	 * @param string $template_name The name of the template to render (without .php)
+	 * @param array  $attributes    The PHP variables for the template
+	 *
+	 * @return string               The contents of the template.
+	 */
+	private function get_template_html( $template_name, $attributes = null ) {
+		if ( ! $attributes ) {
+			$attributes = array();
+		}
+
+		ob_start();
+
+		do_action( 'personalize_login_before_' . $template_name );
+
+		require( 'templates/' . $template_name . '.php');
+
+		do_action( 'personalize_login_after_' . $template_name );
+
+		$html = ob_get_contents();
+		ob_end_clean();
+
+		return $html;
+	}
+
+
+	//
+	// HELPER FUNCTIONS
+	//
+
+	/**
+	 * Finds and returns a matching error message for the given error code.
+	 *
+	 * @param string $error_code    The error code to look up.
+	 *
+	 * @return string               An error message.
+	 */
+	private function get_error_message( $error_code ) {
+		switch ( $error_code ) {
+			// Login errors
+
+			case 'empty_username':
+				return __( 'You do have an email address, right?', 'personalize-login' );
+
+			case 'empty_password':
+				return __( 'You need to enter a password to login.', 'personalize-login' );
+
+			case 'invalid_username':
+				return __(
+					"We don't have any users with that email address. Maybe you used a different one when signing up?",
+					'personalize-login'
+				);
+
+			case 'incorrect_password':
+				$err = __(
+					"The password you entered wasn't quite right. <a href='%s'>Did you forget your password</a>?",
+					'personalize-login'
+				);
+				return sprintf( $err, wp_lostpassword_url() );
+
+			default:
+				break;
+		}
+
+		return __( 'An unknown error occurred. Please try again later.', 'personalize-login' );
+	}
+
+}
+
+// Initialize the plugin
+$personalize_login_pages_plugin = new Personalize_Login_Plugin();
+
+// Create the custom pages at plugin activation
+register_activation_hook( __FILE__, array( 'Personalize_Login_Plugin', 'plugin_activated' ) );
