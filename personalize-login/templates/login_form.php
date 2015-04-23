822f6e0b6441446be2f62e81c9382f24be0edc0f
Jarkko Laine <jarkko@jarkkolaine.com>
2015-04-23 14:21:27 +0300

    Removed unnecessary code

--- personalize-login/templates/login_form.php
+++ personalize-login/templates/login_form.php
@@ -19,29 +19,6 @@
 		</p>
 	<?php endif; ?>
 
-	<?php if ( isset( $attributes['registered'] ) && $attributes['registered'] ) : ?>
-		<p class="login-info">
-			<?php
-				printf(
-					__( 'You have successfully registered to <strong>%s</strong>. We have emailed your password to the email address you entered.', 'personalize-login' ),
-					get_bloginfo( 'name' )
-				);
-			?>
-		</p>
-	<?php endif; ?>
-
-	<?php if ( $attributes['lost_password_sent'] ) : ?>
-		<p class="login-info">
-			<?php _e( 'Check your email for a link to reset your password.', 'personalize-login' ); ?>
-		</p>
-	<?php endif; ?>
-
-	<?php if ( $attributes['password_updated'] ) : ?>
-		<p class="login-info">
-			<?php _e( 'Your password has been changed. You can sign in now.', 'personalize-login' ); ?>
-		</p>
-	<?php endif; ?>
-
 	<?php
 		wp_login_form(
 			array(
