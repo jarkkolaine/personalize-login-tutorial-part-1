b832c2db48ca55f7becfe39c3fc90104da4b2292
Jarkko Laine <jarkko@jarkkolaine.com>
2015-04-23 14:20:18 +0300

    Removed unnecessary code

--- personalize-login/templates/login_form.php
+++ personalize-login/templates/login_form.php
@@ -1,4 +1,3 @@
-<?php if ( true ) : ?>
 <div class="login-form-container">
 	<?php if ( $attributes['show_title'] ) : ?>
 		<h2><?php _e( 'Sign In', 'personalize-login' ); ?></h2>
@@ -58,20 +57,3 @@
 	</a>
 
 </div>
-<?php else : ?>
-	<div class="login-form-container">
-		<form method="post" action="<?php echo wp_login_url(); ?>">
-			<p class="login-username">
-				<label for="user_login"><?php _e( 'Email', 'personalize-login' ); ?></label>
-				<input type="text" name="log" id="user_login">
-			</p>
-			<p class="login-password">
-				<label for="user_pass"><?php _e( 'Password', 'personalize-login' ); ?></label>
-				<input type="password" name="pwd" id="user_pass">
-			</p>
-			<p class="login-submit">
-				<input type="submit" value="<?php _e( 'Sign In', 'personalize-login' ); ?>">
-			</p>
-		</form>
-	</div>
-<?php endif; ?>
